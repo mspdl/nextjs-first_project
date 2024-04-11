7c0a836e0e0946920528b039dfd929bc65f4d5db
Morgan <morgan.espindola@gmail.com>
2024-04-11 20:13:02 -0300

    How does Client and Server work on NextJS

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import { useState } from "react";
 
 const About = () => {
+  const [count, setCount] = useState(110);
+
   return (
     <div>
-      <h1 className="p-5 text-5xl">About page</h1>
+      <h1 className="p-5 text-5xl">About page ({count})</h1>
       <ul className="list-disc list-inside">
         <li>
           <Link href="/about/morgan">Morgan</Link>
@@ -12,6 +15,8 @@ const About = () => {
           <Link href="/about/john">John</Link>
         </li>
       </ul>
+
+      <button onClick={() => setCount(count + 1)}>increase</button>
     </div>
   );
 };
