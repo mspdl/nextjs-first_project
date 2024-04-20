a39b16dc8668544a9cd45111aaaa0c047c0dadcb
Morgan <morgan.espindola@gmail.com>
2024-04-19 21:07:07 -0300

    FastRefresh

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,11 +2,14 @@ import Link from "next/link";
 import { useState } from "react";
 
 const About = () => {
-  const [count, setCount] = useState(110);
+  const DEFAULT_VALUE = 110;
+  const [count, setCount] = useState(DEFAULT_VALUE);
 
   return (
     <div>
       <h1 className="p-5 text-5xl">About page ({count})</h1>
+      <p>Started in {DEFAULT_VALUE}</p>
+      {/* Only refresh the component (p) above (the only that changed) but keeps the state*/}
       <ul className="list-disc list-inside">
         <li>
           <Link href="/about/morgan">Morgan</Link>
@@ -15,7 +18,6 @@ const About = () => {
           <Link href="/about/john">John</Link>
         </li>
       </ul>
-
       <button onClick={() => setCount(count + 1)}>increase</button>
     </div>
   );
