9796d53ee226d9e416adbfd7dd94923c28c47227
Morgan <morgan.espindola@gmail.com>
2024-04-20 22:00:53 -0300

    Environment Variables in NextJS

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { useState } from "react";
 
-const About = () => {
+type Props = {
+  name: string;
+};
+
+const About = ({ name }: Props) => {
   const DEFAULT_VALUE = 110;
   const [count, setCount] = useState(DEFAULT_VALUE);
 
@@ -10,6 +14,8 @@ const About = () => {
       <h1 className="p-5 text-5xl">About page ({count})</h1>
       <p>Started in {DEFAULT_VALUE}</p>
       {/* Only refresh the component (p) above (the only that changed) but keeps the state*/}
+      <p>My name is {process.env.NEXT_PUBLIC_NAME}</p>
+      <p>This text: {name}</p>
       <ul className="list-disc list-inside">
         <li>
           <Link href="/about/morgan">Morgan</Link>
@@ -21,6 +27,14 @@ const About = () => {
       <button onClick={() => setCount(count + 1)}>increase</button>
     </div>
   );
+};
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      name: process.env.NAME,
+    },
+  };
 };
 
 export default About;
