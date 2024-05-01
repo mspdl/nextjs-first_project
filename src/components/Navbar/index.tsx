6c5b72ac6a567868fc2078703e2abb11a8b4049d
Morgan <morgan.espindola@gmail.com>
2024-05-01 20:32:37 -0300

    Creating navigation menu in NextJS - part 2

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,21 @@
 import { navigationLinks } from "@/utils/data";
 import Link from "next/link";
 import styles from "./Navbar.module.css";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <ul className={styles.container}>
       {navigationLinks.map((link, index) => (
-        <li key={index} className={styles.linkItem}>
+        <li
+          key={index}
+          className={`
+            ${styles.linkItem} ${
+            router.pathname === link.path ? styles.linkActive : ""
+          }`}
+        >
           <Link href={link.path}>{link.label}</Link>
         </li>
       ))}
