4da0e142206a5f50df56798e037f3d81c2466e8e
Morgan <morgan.espindola@gmail.com>
2024-05-13 19:27:43 -0300

    Dealing with CORS in NextJS

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { domains: ["www.google.com.br"] },
+  headers: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
