9263ba6920621266a574130ac5ca5510e0990a59
Morgan <morgan.espindola@gmail.com>
2024-05-15 20:44:12 -0300

    Dealing with different methods

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,8 +1,22 @@
 import { Users } from "@/utils/users";
 import { NextApiHandler } from "next";
 
-const handler: NextApiHandler = (req, res) => {
+const getAllUsers: NextApiHandler = async (req, res) => {
   res.json(Users);
+};
+const insertNewUser: NextApiHandler = async (req, res) => {
+  res.json({ status: true });
+};
+
+const handler: NextApiHandler = (req, res) => {
+  switch (req.method) {
+    case "GET":
+      getAllUsers(req, res);
+      break;
+    case "POST":
+      insertNewUser(req, res);
+      break;
+  }
 };
 
 export default handler;
