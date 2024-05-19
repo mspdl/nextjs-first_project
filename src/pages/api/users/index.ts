f85628f118dd5fbaa81cbd83a4418f87ec2a5c87
Morgan <morgan.espindola@gmail.com>
2024-05-19 15:48:53 -0300

    API return status

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -2,12 +2,12 @@ import { Users } from "@/utils/users";
 import { NextApiHandler } from "next";
 
 const handlerGet: NextApiHandler = async (req, res) => {
-  res.json(Users);
+  res.status(200).json(Users);
 };
 
 const handlerPost: NextApiHandler = async (req, res) => {
   const { name, age } = req.body;
-  res.json({ status: true, user: { name, age } });
+  res.status(201).json({ status: true, user: { name, age } });
 };
 
 const handler: NextApiHandler = (req, res) => {
