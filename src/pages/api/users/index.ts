84fcfd149c31e6f9d8ef87c4d578929553d954e9
Morgan <morgan.espindola@gmail.com>
2024-05-24 23:22:45 -0300

    Prisma: Entering data

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,13 +1,17 @@
 import { Users } from "@/utils/users";
 import { NextApiHandler } from "next";
+import prisma from "../../../../libs/prisma";
 
 const handlerGet: NextApiHandler = async (req, res) => {
   res.status(200).json(Users);
 };
 
 const handlerPost: NextApiHandler = async (req, res) => {
-  const { name, age } = req.body;
-  res.status(201).json({ status: true, user: { name, age } });
+  const { name, email } = req.body;
+
+  const newUser = await prisma.user.create({ data: { name, email } });
+
+  res.status(201).json({ status: true, user: newUser });
 };
 
 const handler: NextApiHandler = (req, res) => {
