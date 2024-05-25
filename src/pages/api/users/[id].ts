604945b78943106d83afa2c780b752ea13006179
Morgan <morgan.espindola@gmail.com>
2024-05-25 19:30:56 -0300

    Prism: Selecting data 2

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,10 +1,16 @@
-import { MockUsers } from "@/utils/users";
 import { NextApiHandler } from "next";
+import prisma from "../../../../libs/prisma";
 
-const handler: NextApiHandler = (req, res) => {
+const handler: NextApiHandler = async (req, res) => {
   const urlUserId = req.query.id;
-  const currentUser = MockUsers.find((user) => user.id?.toString() === urlUserId);
-  currentUser ? res.json(currentUser) : res.json({ error: "User not found" });
+
+  const user = await prisma.user.findFirst({
+    where: { id: parseInt(urlUserId as string), active: true },
+  });
+
+  user
+    ? res.json({ status: true, user })
+    : res.status(404).json({ error: "User not found" });
 };
 
 export default handler;
