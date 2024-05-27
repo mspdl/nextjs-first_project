9224e3ede96e03e6112a6fb935aa6a88c0a8b1d5
Morgan <morgan.espindola@gmail.com>
2024-05-26 23:28:40 -0300

    Prisma: Changing data 1

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,7 +1,7 @@
 import { NextApiHandler } from "next";
 import prisma from "../../../../libs/prisma";
 
-const handler: NextApiHandler = async (req, res) => {
+const handlerGet: NextApiHandler = async (req, res) => {
   const urlUserId = req.query.id;
 
   const user = await prisma.user.findFirst({
@@ -11,6 +11,35 @@ const handler: NextApiHandler = async (req, res) => {
   user
     ? res.json({ status: true, user })
     : res.status(404).json({ error: "User not found" });
+};
+
+const handlerPut: NextApiHandler = async (req, res) => {
+  const { name, active } = req.body;
+  const { id } = req.query;
+
+  let user, error;
+
+  try {
+    user = await prisma.user.update({
+      where: { id: parseInt(id as string) },
+      data: { name, active: active === "true" ? true : false },
+    });
+  } catch (dbError) {
+    error = dbError;
+  }
+
+  user ? res.status(200).json({ user }) : res.status(404).json({ error });
+};
+
+const handler: NextApiHandler = (req, res) => {
+  switch (req.method) {
+    case "GET":
+      handlerGet(req, res);
+      break;
+    case "PUT":
+      handlerPut(req, res);
+      break;
+  }
 };
 
 export default handler;
