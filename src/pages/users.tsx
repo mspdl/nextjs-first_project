71894e0864aa894fe4c3b8bb721e9ad557cecbee
Morgan <morgan.espindola@gmail.com>
2024-06-18 13:55:03 -0300

    Using the API on Client-side - part 1

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,6 +1,7 @@
 import { Layout } from "@/components/Layout";
 import { User } from "@/types/User";
 import Head from "next/head";
+import { useState } from "react";
 import api from "../../libs/api";
 
 type Props = {
@@ -8,6 +9,27 @@ type Props = {
 };
 
 const Users = ({ users }: Props) => {
+  const [showMore, setShowMore] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [pageCount, setPageCount] = useState(1);
+  const [userList, setUserList] = useState<User[]>(users);
+
+  const handleLoadMore = async () => {
+    if (!loading) {
+      setLoading(true);
+      const req = await fetch(`/api/users?page=${pageCount + 1}`);
+      const json = await req.json();
+      if (json.status) {
+        if (json.users.length < 3) {
+          setShowMore(false);
+        }
+        setUserList([...userList, ...json.users]);
+      }
+      setLoading(false);
+      setPageCount(pageCount + 1);
+    }
+  };
+
   return (
     <Layout>
       <div>
@@ -17,10 +39,27 @@ const Users = ({ users }: Props) => {
         <h1 className="text-3xl font-bold text-center">Users Page</h1>
 
         <ul>
-          {users.map((user, index) => (
-            <li key={index}>{user.name}</li>
+          {userList.map((user, index) => (
+            <li key={index}>
+              <p>
+                {index + 1} - {user.name}{" "}
+                <span className="italic text-xs">({user.email})</span>
+                <span className="text-xs"> [{user.id}]</span>
+              </p>
+            </li>
           ))}
         </ul>
+
+        {loading && "loading"}
+
+        {showMore && (
+          <button
+            onClick={handleLoadMore}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Load more
+          </button>
+        )}
       </div>
     </Layout>
   );
