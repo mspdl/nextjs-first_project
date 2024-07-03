437d5a6e90abb5151835194dca8e883628976322
Morgan <morgan.espindola@gmail.com>
2024-07-03 10:52:31 -0300

    Creating your own login form - part 2

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,42 @@
 import { Layout } from "@/components/Layout";
 import { signIn } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
 
   const handleSubmit = async () => {
+    if (!email || !password) {
+      setErrorMessage("Please fill in the email and password fields");
+      return;
+    }
+
+    setErrorMessage("");
+    setLoading(true);
     const request = await signIn("credentials", {
       redirect: false,
       email,
       password,
     });
+
+    setLoading(false);
+
+    if (request && request.ok) {
+      if (router.query.callbackUrl) {
+        router.push(router.query.callbackUrl as string);
+      } else {
+        router.push("/");
+      }
+    } else {
+      setErrorMessage(`Error: ${request?.error} | status: ${request?.status}`);
+    }
   };
 
   return (
@@ -32,6 +56,7 @@ const Login = () => {
               onChange={(e) => {
                 setEmail(e.target.value);
               }}
+              disabled={loading}
             />
             <input
               className="p-2 rounded-md text-black"
@@ -41,14 +66,20 @@ const Login = () => {
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
+              disabled={loading}
             />
           </div>
           <button
             onClick={handleSubmit}
             className="p-1 rounded-md bg-blue-300 my-3 w-72 text-white hover:bg-blue-500 hover:text-black transition ease-in-out delay-100"
+            disabled={loading}
           >
             Login
           </button>
+          {errorMessage && (
+            <p className="font-bold text-red-600">{errorMessage}</p>
+          )}
+          {loading && <p>Loading...</p>}
         </div>
       </Layout>
     </div>
