e976224f2513d9fc3ed037664acae02af7d600cd
Morgan <morgan.espindola@gmail.com>
2024-07-03 07:19:56 -0300

    Login: Private pages (via client-side)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Layout } from "@/components/Layout";
 import { User } from "@/types/User";
 import axios from "axios";
+import { signIn, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
@@ -11,6 +12,8 @@ type Props = {
 };
 
 const Users = ({ users }: Props) => {
+  const { data: session, status: sessionStatus } = useSession();
+
   const [showMore, setShowMore] = useState(true);
   const [loading, setLoading] = useState(false);
   const [pageCount, setPageCount] = useState(1);
@@ -38,36 +41,53 @@ const Users = ({ users }: Props) => {
         <Head>
           <title>Users</title>
         </Head>
+
         <h1 className="text-3xl font-bold text-center">Users Page</h1>
 
-        <Link
-          className="p-1 border border-blue-500 rounded-md bg-blue-300"
-          href={`/users/new`}
-        >
-          New User
-        </Link>
+        {sessionStatus === "loading" && <div>Loading</div>}
+        {sessionStatus === "unauthenticated" && (
+          <div>
+            <p>{`You're not allowed to access this page`}</p>
+            <button
+              className="border border-white rounded-md p-3 mb-10"
+              onClick={() => signIn()}
+            >
+              Login
+            </button>
+          </div>
+        )}
+        {sessionStatus === "authenticated" && (
+          <>
+            <Link
+              className="p-1 border border-blue-500 rounded-md bg-blue-300"
+              href={`/users/new`}
+            >
+              New User
+            </Link>
 
-        <ul>
-          {userList.map((user, index) => (
-            <li key={index}>
-              <p>
-                {index + 1} - {user.name}{" "}
-                <span className="italic text-xs">({user.email})</span>
-                <span className="text-xs"> [{user.id}]</span>
-              </p>
-            </li>
-          ))}
-        </ul>
+            <ul>
+              {userList.map((user, index) => (
+                <li key={index}>
+                  <p>
+                    {index + 1} - {user.name}{" "}
+                    <span className="italic text-xs">({user.email})</span>
+                    <span className="text-xs"> [{user.id}]</span>
+                  </p>
+                </li>
+              ))}
+            </ul>
 
-        {loading && "loading"}
+            {loading && "loading"}
 
-        {showMore && !loading && (
-          <button
-            onClick={handleLoadMore}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Load more
-          </button>
+            {showMore && !loading && (
+              <button
+                onClick={handleLoadMore}
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Load more
+              </button>
+            )}
+          </>
         )}
       </div>
     </Layout>
