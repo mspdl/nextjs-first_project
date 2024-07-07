edc80e62ccffb6eaa7235acde3b46f042c4d91ee
Morgan <morgan.espindola@gmail.com>
2024-07-06 23:28:43 -0300

    Login via Google

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import api from "../../../../libs/api";
 
 export const authOptions: NextAuthOptions = {
@@ -33,6 +34,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    })
   ],
   callbacks: {
     jwt: async ({ token, user }) => {
@@ -49,7 +54,7 @@ export const authOptions: NextAuthOptions = {
     },
   },
   pages: {
-    signIn: "/login",
+    // signIn: "/login",
   },
 };
 
