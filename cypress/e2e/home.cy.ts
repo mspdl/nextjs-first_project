e9a27ad2024c23912b25f7d703c403d1715bfe94
Morgan <morgan.espindola@gmail.com>
2024-07-09 22:18:32 -0300

    Creating first e2e tests with Cypress - part 2

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -12,4 +12,15 @@ describe("Home Page", () => {
     cy.visit("/pt");
     cy.get("header").should("not.contain", "Choosen Language: en");
   });
+
+  it("should load page in Portuguese", () => {
+    cy.visit("/pt");
+    cy.get("header").should("contain", "Lingaguem selecionada: pt");
+  });
+
+  it("should go to login page", () => {
+    cy.visit("/");
+    cy.contains("Log In").click();
+    cy.url().should('include', '/api/auth/signin')
+  });
 });
